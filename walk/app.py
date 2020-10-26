dcbc0239a4cb7d1b15acd753037bdca90cb95fd0
lvicen3 <57845715+lvicen3@users.noreply.github.com>
2020-10-26 04:19:11 -0500

    heroku fix imports

--- walk/app.py
+++ walk/app.py
@@ -8,6 +8,41 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Function to split a document into a list of tokens
+# Arguments:
+# doc: A string containing input document
+# Returns: tokens (list)
+# Where, tokens (list) is a list of tokens that the document is split into
+def get_tokens(doc):
+	tokens = re.split(r"[^A-Za-z0-9-']", doc)
+	tokens = list(filter(len, tokens))
+	return tokens
+
+
+# Function to split a document into a list of tokens
+# Arguments: None
+# Returns: stopwords (set)
+# Where, stopwords (set) is a list of stopwords to be filtered from the document
+def get_stopwords():
+    stopwords = set(['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're",
+                    "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he',
+                    'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's",
+                    'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which',
+                    'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was',
+                    'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did',
+                    'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while',
+                    'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during',
+                    'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off',
+                    'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why',
+                    'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor',
+                    'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don',
+                    "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren',
+                    "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn',
+                    "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't",
+                    'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't",
+                    'won', "won't", 'wouldn', "wouldn't"])
+    return stopwords
+
 stopwords = get_stopwords()	
 bands = ['Vampire_Weekend', 'Radiohead', 'Interpol','MGMT','The_Strokes']
 N_LINES = 3
@@ -219,5 +254,5 @@ def fifth_band():
 
     return render_template('fifth_band.html', band_names=bands[4], lines = lines,img_url=img_url)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
